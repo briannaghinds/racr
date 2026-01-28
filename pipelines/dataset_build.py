fe162a11454ed306adfbc703e9b038bcb4187ca9
Brianna Hinds <briden32016@gmail.com>
2026-01-28 13:41:35 -0600

    cleaned up the data pipeline

--- pipelines/dataset_build.py
+++ pipelines/dataset_build.py
@@ -15,7 +15,8 @@ def initialize_data(year: int) -> pd.DataFrame:
         year: specified season year
     """
 
-    # loop over all races
+    # loop over all races in a season
+    # NOTE: i don't think 2025 is fully on the fastf1 databases so I am pulling all of 2024
     # fastf1.Cache.enable_cache("cache/fastf1")
     all_laps = []
     schedule = fastf1.get_event_schedule(year)
@@ -32,7 +33,7 @@ def initialize_data(year: int) -> pd.DataFrame:
             laps["year"] = year
             laps["round"] = round_no
             laps["track"] = race_name
-            laps["race_id"] = str(year) + str(round_no) + str(race_name.replace(" ", "_"))
+            laps["race_id"] = f"{year}_{round_no:02d}_{race_name.replace(' ', '_')}"
 
             all_laps.append(laps)
             print(f"Loaded {race_name}")
@@ -44,7 +45,7 @@ def initialize_data(year: int) -> pd.DataFrame:
     return laps
 
 
-def lap_times_data(laps: pd.DataFrame):
+def lap_times_data(laps: pd.DataFrame) -> pd.DataFrame:
     """
     Returns lap_times table pulled from the combined laps dataframe.
 
@@ -59,7 +60,7 @@ def lap_times_data(laps: pd.DataFrame):
         "LapNumber": "lap_number",
         "Stint": "stint",
         "Compound": "compound",
-        "TyreLife": "tyre_age",
+        "TyreLife": "tire_age",
         "LapTime": "lap_time"
         })
 
@@ -73,10 +74,17 @@ def lap_times_data(laps: pd.DataFrame):
     # drop helper columns
     lap_times_df = lap_times_df.drop(columns=["lap_time", "PitInTime", "PitOutTime"])
 
+    # ML sanity filter
+    lap_times_df = lap_times_df[
+        (lap_times_df["lap_time_sec"].notna())
+        & (~lap_times_df["is_inlap"])
+        & (~lap_times_df["is_outlap"])
+    ]
+
     return lap_times_df 
 
 
-def race_data(laps: pd.DataFrame):
+def race_data(laps: pd.DataFrame) -> pd.DataFrame:
     """
     Returns races table (one row per race).
 
@@ -96,6 +104,19 @@ def race_data(laps: pd.DataFrame):
 
     return races_df
 
+
+def stint_data(lap_times: pd.DataFrame) -> pd.DataFrame:
+    stints_df = lap_times.groupby(
+        ["race_id", "driver", "stint", "compound"]).agg(
+            start_lap=("lap_number", "min"),
+            end_lap=("lap_number", "max"),
+            stint_length=("lap_number", "count"),
+            avg_lap_time=("lap_time_sec", "mean")
+    ).reset_index()
+
+    return stints_df
+
+
 def driver_data(laps: pd.DataFrame) -> pd.DataFrame:
     drivers_df = (
         laps[["Driver", "Team"]].drop_duplicates().rename(
@@ -103,10 +124,11 @@ def driver_data(laps: pd.DataFrame) -> pd.DataFrame:
                 "Driver": "driver", 
                 "Team": "team"
             }
-        ).reset_index()
+        ).reset_index(drop=True)
     )
 
     return drivers_df
+
 
 def tire_compounds_data(laps: pd.DataFrame) -> pd.DataFrame:
     """
@@ -122,7 +144,9 @@ def tire_compounds_data(laps: pd.DataFrame) -> pd.DataFrame:
         "deg_rate": [0.01]*len(compounds),   # placeholder
         "cliff_lap": [20]*len(compounds)     # placeholder
     })
+
     return tire_df
+
 
 def race_conditions_data(laps: pd.DataFrame) -> pd.DataFrame:
     """
@@ -132,13 +156,14 @@ def race_conditions_data(laps: pd.DataFrame) -> pd.DataFrame:
         laps: concatenated laps dataframe
     """
 
-    weather_cols = ["TrackTemp", "AirTemp", "Rainfall"]
-
     weather_df = (
-        laps[["race_id"] + weather_cols]
-        .dropna(subset=weather_cols)
+        laps[["race_id", "TrackTemp", "AirTemp", "Rainfall"]]
         .groupby("race_id")
-        .first()
+        .agg({
+            "TrackTemp": "mean",
+            "AirTemp": "mean",
+            "Rainfall": "max"
+        })
         .reset_index()
         .rename(columns={
             "TrackTemp": "track_temp",
@@ -150,28 +175,70 @@ def race_conditions_data(laps: pd.DataFrame) -> pd.DataFrame:
     return weather_df
 
 
+def track_data() -> pd.DataFrame:
+    """
+    Manually created DataFrame for each circuit.
+    Returns DataFrame object of the circuit information.
+    """
+    # data pulled from https://motorsporttickets.com/blog/how-many-laps-does-each-formula-1-race-have/
+    data = {
+        "track": ["Australian Grand Prix", "Chinese Grand Prix", "Japanese Grand Prix", "Bahrain Grand Prix", "Saudi Arabia Grand Prix", "Miami Grand Prix", "Canadian Grand Prix", "Monaco Grand Prix", "Barcelona Grand Prix", "Austrian Grand Prix", "British Grand Prix", "Belgian Grand Prix", "Hungarian Grand Prix", "Dutch Grand Prix", "Italian Grand Prix", "Spanish Grand Prix", "Azerbaijan Grand Prix", "Singapore Grand Prix", "United States Grand Prix", "Mexican Grand Prix", "Brazilian Grand Prix", "Las Vegas Grand Prix", "Qatar Grand Prix", "Abu Dhabi Grand Prix"],
+        "circuit": ["Melbourne Grand Prix Circuit", "Shanghai International Circuit", "Suzuka", "Bahrain International Circuit", "Jeddah Street Circuit", "Hard Rock Stadium Circuit", "Circuit Gilles Villeneuve", "Circuit de Monaco", "Circuit de Barcelona-Catalunya", "Red Bull Ring", "Silverstone Circuit", "Circuit de Spa-Francorchamps", "Hangaroring", "Zandvoort", "Autodromo Nazionale di Monza", "Madring Circuit", "Baku City Circuit", "Marina Bay Street Circuit", "Circuit of the Americas", "Autodromo Hermanos Rodriguez", "Autodromo Jose Carlos Pace", "Las Vegas Street Circuit", "Lusail Circuit", "Yas Marina Circuit"],
+        "circuit_length (km)": [5.303, 5.451, 5.807, 5.412, 6.175, 5.41, 4.361, 3.337, 4.655, 4.318, 5.891, 7.004, 4.381, 4.259, 5.793, 5.474, 6.003, 5.063, 5.513, 4.304, 4.309, 6.201, 5.419, 5.554],
+        "race_distance (km)": [307.574, 305.066, 307.471, 308.238, 308.750, 308.37, 305.270, 260.286, 307.104, 306.452, 306.198, 308.052, 306.630, 306.648, 306.720, 312.018, 306.049, 308.706, 308.405, 305.354, 305.879, 310.05, 308.826, 305.355],
+        "laps": [58, 56, 53, 57, 50, 57, 70, 78, 66, 71, 52, 44, 70, 72, 53, 57, 51, 61, 56, 71, 71, 50, 57, 55] 
+    }
+
+    track_df = pd.DataFrame(data)
+
+    return track_df
+
+
+
 
 ## MAIN
 if __name__ == "__main__":
-    # initialize data
-    laps = initialize_data(2024)  # 2025 seems to have no data
-    laps.to_csv("../data/concatenated_laps_df.csv")
+    # initialize data from api
+    laps = initialize_data(2024)  # 2025 seems to have no data or either incomplete data
 
+    ## DATAFRAME CREATIONS ##
     # create lap information
-    lap_time_df = lap_times_data(laps)
-    lap_time_df.to_csv("../data/lap_time_df.csv")
+    lap_times_df = lap_times_data(laps)
 
     # create race table
     races_df = race_data(laps)
-    races_df.to_csv("../data/races_df.csv")
+
+    # create stint table
+    stint_df = stint_data(lap_times_df)
 
     # create tire compounds data
     tire_compounds_df = tire_compounds_data(laps)
-    tire_compounds_df.to_csv("../data/tire_compounds_df.csv")
 
+    # create drivers table
     driver_df = driver_data(laps)
-    driver_df.to_csv("../data/drivers_df.csv")
 
     # create race conditions table
-    race_conditions_df = race_conditions_data()
-    race_conditions_df.to_csv("./data/race_conditions_df.csv")
+    race_conditions_df = race_conditions_data(laps)
+
+    # create track table
+    track_df = track_data()
+
+    ## ASSERTIONS (sanity check) ##
+    # assert lap_time_df["lap_time_sec"].min() > 60
+    # assert lap_time_df["lap_time_sec"].max() < 200
+
+    ## CSV CREATIONS ##
+    laps.to_csv("../data/concatenated_laps_df.csv", index=False)
+    lap_times_df.to_csv("../data/lap_time_df.csv", index=False)
+    races_df.to_csv("../data/races_df.csv", index=False)
+    stint_df.to_csv("../data/stint_df.csv", index=False)
+    tire_compounds_df.to_csv("../data/tire_compounds_df.csv", index=False)
+    driver_df.to_csv("../data/drivers_df.csv", index=False)
+    race_conditions_df.to_csv("./data/race_conditions_df.csv", index=False)
+    track_df.to_csv("../data/track_df.csv", index=False)
+
+
+
+
+
+
