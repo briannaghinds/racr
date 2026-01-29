b30dbfd2a53ee8d883ad309bc1eaa1a9511b3345
Brianna Hinds <briden32016@gmail.com>
2026-01-28 21:17:30 -0600

    got all data tables

--- pipelines/dataset_build.py
+++ pipelines/dataset_build.py
@@ -31,6 +31,21 @@ def initialize_data(year: int) -> pd.DataFrame:
             session.load(laps=True, weather=True)
 
             laps = session.laps.copy()
+            weather = session.weather_data.copy()
+
+            weather = weather.sort_values("Time")
+            laps = laps.sort_values("LapStartTime")
+
+            laps = pd.merge_asof(
+                laps,
+                weather,
+                left_on="LapStartTime",
+                right_on="Time",
+                direction="nearest"
+            )
+
+            # laps = session.laps.copy()
+            # add metadata after merge
             laps["year"] = year
             laps["round"] = round_no
             laps["track"] = race_name
@@ -109,9 +124,9 @@ def race_data(laps: pd.DataFrame) -> pd.DataFrame:
 def stint_data(lap_times: pd.DataFrame) -> pd.DataFrame:
     stints_df = lap_times.groupby(
         ["race_id", "driver", "stint", "compound", "track"]).agg(
-            start_lap=("lap_number", "min"),
-            end_lap=("lap_number", "max"),
-            stint_length=("lap_number", "count"),
+            start_lap=("race_lap", "min"),
+            end_lap=("race_lap", "max"),
+            stint_length=("race_lap", "count"),
             avg_lap_time=("lap_time_sec", "mean")
     ).reset_index()
 
@@ -208,6 +223,7 @@ def track_data() -> pd.DataFrame:
 if __name__ == "__main__":
     # initialize data from api
     laps = initialize_data(2024)  # 2025 seems to have no data or either incomplete data
+    print(laps.columns)
 
     ## DATAFRAME CREATIONS ##
     # create lap information
@@ -237,18 +253,18 @@ if __name__ == "__main__":
 
     ## TABLE COMBINATIONS ##
     # combine the track information for each lap_time_df
-    lap_times_df = pd.merge(
+    lap_times_df = lap_times_df.merge(
         track_df[["track", "circuit_length(km)"]],
         on="track",
         how="left"
     )
 
-    ## CSV CREATIONS ##
-    laps.to_csv("../data/concatenated_laps_df.csv", index=False)
-    lap_times_df.to_csv("../data/lap_time_df.csv", index=False)
-    races_df.to_csv("../data/races_df.csv", index=False)
-    stint_df.to_csv("../data/stint_df.csv", index=False)
-    tire_compounds_df.to_csv("../data/tire_compounds_df.csv", index=False)
-    driver_df.to_csv("../data/drivers_df.csv", index=False)
-    race_conditions_df.to_csv("../data/race_conditions_df.csv", index=False)
-    track_df.to_csv("../data/track_df.csv", index=False)
+## CSV CREATIONS ##
+laps.to_csv("../data/concatenated_laps_df.csv", index=False)
+lap_times_df.to_csv("../data/lap_time_df.csv", index=False)
+races_df.to_csv("../data/races_df.csv", index=False)
+stint_df.to_csv("../data/stint_df.csv", index=False)
+tire_compounds_df.to_csv("../data/tire_compounds_df.csv", index=False)
+driver_df.to_csv("../data/drivers_df.csv", index=False)
+race_conditions_df.to_csv("../data/race_conditions_df.csv", index=False)
+track_df.to_csv("../data/track_df.csv", index=False)
