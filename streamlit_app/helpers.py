ec6ee057bcdce4c942344c5b7401b8b5c8d96c97
Brianna Hinds <briden32016@gmail.com>
2026-02-01 21:47:52 -0600

    MVP1 of full race simulation

--- streamlit_app/helpers.py
+++ streamlit_app/helpers.py
@@ -4,6 +4,7 @@ Description: helper funcitons for the F1 application
 """
 from constants import INPUT_COLS, DEFAULT_VALS
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def data_cleaning(user_choices, pull_default=True):
@@ -15,8 +16,24 @@ def data_cleaning(user_choices, pull_default=True):
     # make sure input is in order the model expects
     user_choices = user_choices[INPUT_COLS]
 
-    # SANITY PRINTS
-    # print(user_choices)
-    # st.dataframe(user_choices)
-
     return user_choices
+
+def baseline_value(track_choice, total_laps):
+    baseline_df = pd.read_csv("../data/baseline_references.csv")
+    baseline_val = baseline_df.loc[baseline_df["track"] == track_choice, "avg_lap_time"]
+    baseline_val = baseline_val.values[0] * total_laps # access the first thing in the Series
+
+    return baseline_val
+
+def race_explain_visualizer(lap_times: list, pits: list):
+    """
+    Takes the lap times list and creates a line plot that plots lap vs lap time (vertical dashes will be placed on the pit stop laps).
+    Returns a matplotlib object.
+
+    Args
+        lap_times: list of all lap times predicted by the XGBoost model
+        pits: list of laps that were pit stops
+    """
+    # make a line plot (lap vs lap_times)
+
+    pass
--- streamlit_app/simulator.py
+++ streamlit_app/simulator.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from helpers import data_cleaning
+from helpers import data_cleaning, baseline_value, race_explain_visualizer
 from constants import TRACKS, PIT_STOP_LOSS
 import matplotlib.pyplot as plt
 import time
@@ -9,11 +9,10 @@ import numpy as np
 import pandas as pd
 
 # """GOALS
-# - build all the UI first (NO "backend" yet)
-# - make a simple UI with the track (start static) then implement a track drop down
 # - figure out what metrics I want to be changed by the user
 # """
 
+## MODEL LOADING ##
 @st.cache_resource
 def load_model():
     model = xgb.XGBRegressor()
@@ -22,6 +21,7 @@ def load_model():
 
 model = load_model()
 
+## STREAMLIT UI __INIT__ ##
 def intialize_window():
     """
     Setup the basic metadata of the page.
@@ -31,22 +31,9 @@ def intialize_window():
         page_icon=":racing_car:",
         layout="wide"
     )
+    st.title("🏎️ Race Strategy Simulator")
 
-def lap_time_prediction(track_choice, user_choices):
-    baseline_df = pd.read_csv("../data/baseline_references.csv")
-    baseline_val = baseline_df.loc[baseline_df["track"] == track_choice, "avg_lap_time"]
-    baseline_val = baseline_val.values[0]  # access the first thing in the Series
-
-    # get input columns for model
-    X = data_cleaning(user_choices)
-
-    # predict new time based on user values
-    time_prediction = float(model.predict(X))
-    print(time_prediction)  # SANITY PRINT
-
-    # # i want to return: baseline, predicted
-    return baseline_val, time_prediction
-
+## VISUALIZATION ##
 def plot_circuit(track_choice, show_corners=True):
     """
     Pull the circuit information and create a plotly object.
@@ -62,7 +49,7 @@ def plot_circuit(track_choice, show_corners=True):
         x=track_df["X"],
         y=track_df["Y"],
         mode="lines",
-        line=dict(color="white", width=7),
+        line=dict(color="white", width=6),
         hoverinfo="skip"
     ))
 
@@ -82,13 +69,15 @@ def plot_circuit(track_choice, show_corners=True):
     fig.update_layout(
         xaxis=dict(visible=False),
         yaxis=dict(visible=False, scaleanchor="x"),
-        margin=dict(l=0, r=0, t=0, b=0)
+        margin=dict(l=0, r=0, t=0, b=0),
+        paper_bgcolor="black"
     )
 
     fig.update_traces(showlegend=False)
 
     return fig
 
+## SIMULATION LOGIC ##
 def simulate_race(strategy, track, track_length, total_laps, model):
     """
     Simulates a sequence of lap predictions based on different decisions and resets.
@@ -100,20 +89,25 @@ def simulate_race(strategy, track, track_length, total_laps, model):
         model: trained lap-time predictor
     """
     # intialize simulator variables
-    current_compound = strategy[0][1]
+    print(strategy)
+    current_compound = strategy[0]["compound"]
     next_pit_index = 1  # pointer in the strategy list
     tire_age = 0
-    race_time = 0
+    race_time = 0.0
     lap_times = []
+    laps_pit = []
 
     for lap in range(1, total_laps+1):
         tire_age += 1  # age of tire goes up with lap 1:1
 
         # check if current lap needs a pit
-        if next_pit_index < len(strategy) and lap == strategy[next_pit_index][0]:
-            current_compound = strategy[next_pit_index][1]  # next pit index = 1 (at this point)
+        if next_pit_index < len(strategy) and lap == strategy[next_pit_index]["start_lap"]:
+            current_compound = strategy[next_pit_index]["compound"]  # next pit index = 1 (at this point)
             race_time += PIT_STOP_LOSS  # NOTE: add a pit loss (either generated or static)
             next_pit_index += 1  # move the pointer
+            tire_age = 0  # reset tire age
+
+            laps_pit.append(lap)
 
         # build feature inputs
         new_feature_vals = pd.DataFrame(data={
@@ -128,23 +122,27 @@ def simulate_race(strategy, track, track_length, total_laps, model):
         print(features)
 
         # predict lap time
-        lap_time = model.predict(features)[0]
+        lap_time = float(model.predict(features)[0])
         lap_times.append(lap_time)
         race_time += lap_time
 
-    return race_time, lap_times
+    return race_time, lap_times, laps_pit
 
+## MAIN UI BUILD ##
 def build_ui_structure():
     """
     Building the UI of the application.
     """
-    # create page foundation
-    st.title("Lap Time Simulator")
-    st.write("Test your lap strategy!")
-    # st.write("Pick a track, pick a tire, and see ...")
+    # # create page foundation
+    # st.title("Lap Time Simulator")
+    # st.write("Test your lap strategy!")
+    # # st.write("Pick a track, pick a tire, and see ...")
 
-    # create a dropdown for each track
+    # pull specific data used later in workflow
     track_choice = st.selectbox("Select a track:", TRACKS, index=0, placeholder="Select track...")
+    track_df = pd.read_csv("../data/track_df.csv")
+    track_length = track_df.loc[(track_df["track"] == track_choice), "circuit_length(km)"].values[0]
+    total_laps = int(track_df.loc[(track_df["track"] == track_choice), "laps"].values[0])
     show_corners = st.checkbox("Show corner labels", value=True)
 
     # split a column (col1 = track visual, col2 = parameters user will change)
@@ -157,48 +155,58 @@ def build_ui_structure():
 
     # data
     with col2:
-        # tire choice
-        tire_choice = st.radio("Compound", ["SOFT", "MEDIUM", "HARD", "INTERMEDIATE", "WET"], index=1)  # default is MEDIUM
+        st.subheader("Strategy Builder")
 
-        # tire age
-        tire_age = st.slider("Tire Age (laps)", 1, 80, value=1)
-        tire_age_sqrt = tire_age ** 2
+        # intialize user strategy
+        strategy = []
+        current_lap = 1
 
-        # weather (rain/no rain)
-        is_rain = st.checkbox("Rain", value=False)
-        is_rain = int(is_rain)  
+        # number of stints
+        # stint: continous period of time a car stays on track between pit stops
+        num_stints = st.number_input("Number of Stints", 1, 4, 2)
 
-    # display delta changes based on user input
-    track_df = pd.read_csv("../data/track_df.csv")
-    user_choice_df = pd.DataFrame(data={
-        f"compound_{tire_choice}": [1],
-        "tire_age": [tire_age],
-        "tire_age_squared": [tire_age_sqrt],
-        "is_rain": [is_rain],
-        "circuit_length(km)": [track_df.loc[(track_df["track"] == track_choice), "circuit_length(km)"].values[0]]
-    })
+        # # weather (rain/no rain)
+        # is_rain = st.checkbox("Rain", value=False)
+        # is_rain = int(is_rain)  
 
-    base, pred = lap_time_prediction(track_choice, user_choice_df)
-    delta_val = base - pred
-    # delta_val_sec = abs(delta_val)
+        for i in range(num_stints):
+            # tire choice
+            compound = st.selectbox(f"Stint {i+1} compound", ["SOFT", "MEDIUM", "HARD", "INTERMEDIATE", "WET"], key=f"compound_{i}", index=1)  # default is MEDIUM
+            print(compound)
+            start_lap = current_lap
+            strategy.append({
+                "start_lap": start_lap,
+                "compound": compound
+            })
+
+            if i < num_stints - 1: 
+                current_lap = st.slider(
+                    f"Pit after lap (stint {i+1})",
+                    start_lap+1,
+                    total_laps,
+                    start_lap+15,
+                    key=f"pit_{i}"
+                )
+
+    race_time, lap_times_lst, laps_pit_lst = simulate_race(strategy, track_choice, track_length, total_laps, model)
+    base = baseline_value(track_choice, total_laps)
+    delta_val = base - race_time
     delta_val_sec = f"+{delta_val:.2f} sec" if delta_val < 0 else f"-{delta_val:.2f} sec"
 
     # convert to a time format
-    base = time.strftime("%M:%S", time.gmtime(base))
-    pred = time.strftime("%M:%S", time.gmtime(pred))
+    base = time.strftime("%H:%M:%S", time.gmtime(base))
+    pred = time.strftime("%H:%M:%S", time.gmtime(race_time))
+    st.markdown(f"**Baseline value calculated ({base}), assumes flat average lap time, no pit stops.")
 
-    st.write("after predictions", base, pred)  # SANITY PRINT
+    st.write("after predictions", base, race_time)  # SANITY PRINT
 
     # write metric object to show change
     col1, col2 = st.columns(2)
     col1.metric("Lap Time Change", value=base, delta=delta_val_sec, border=True, delta_color="inverse")
     col2.metric("Predicted Lap Time", value=pred, delta=delta_val_sec, delta_color="inverse", border=True)
 
-
-
-
-
-
+    # strategy explaination
+    fig = race_explain_visualizer(lap_times_lst, laps_pit_lst)
 
 ## MAIN ##
 # initialize page 
